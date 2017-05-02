19219e6a06ab65d81dc561f7f250e76a43be6b71
eracah <ejracah@gmail.com>
2017-05-02 12:43:23 -0700

    cleaned up some of the setting up custer functionality

--- setup_clusters.py
+++ setup_clusters.py
@@ -1,81 +1,111 @@
 
-# coding: utf-8
 
-# In[ ]:
 
 import tensorflow as tf
 
 
-# In[ ]:
 
 import sys
-# from nbfinder import NotebookFinder
-# sys.meta_path.append(NotebookFinder())
-from util import expand_nodelist
 import os
-import random
 
-# In[ ]:
 
-def setup_slurm_cluster():
-    tf_hostlist = [ ("%s:22222" % node) for node in
-                expand_nodelist( os.environ['SLURM_NODELIST']) ] 
-    ps_hosts = [tf_hostlist[0]]
-    print ps_hosts
-    worker_hosts = tf_hostlist[1:]
-    print worker_hosts
-    task_index  = int( os.environ['SLURM_PROCID'] )
-    n_tasks     = int( os.environ['SLURM_NPROCS'] )
-    print task_index
+
+"""Works for tensorflow 0.12 for now"""
+def setup_slurm_cluster(num_ps=1):
+    all_nodes = get_all_nodes()
+    port = get_allowed_port()
     
-    cluster = tf.train.ClusterSpec({
+    tf_hostlist = [ ("%s:%i" % (node, port)) for node in all_nodes ] 
+    ps_hosts, worker_hosts = get_parameter_server_and_worker_hosts(hostlist, num_ps=num_ps)
+
+
+    proc_id, num_procs = get_slurm_proc_variables()
+    
+    num_tasks = num_procs - num_ps
+    
+    job_name = get_job_name(proc_id, num_ps)
+    
+    task_index = get_task_index(proc_id, job_name, num_ps)
+    
+    cluster_spec = make_cluster_spec(worker_hosts, ps_hosts)
+
+    server = make_server(cluster_spec, job_name, task_index)
+    
+    return cluster, server, task_index, num_tasks, job_name
+    
+
+def make_server(cluster_spec, job_name, task_index):
+    server = tf.train.Server(cluster_spec,
+                           job_name=job_name,
+                           task_index=task_index)
+    return server
+    
+    
+def make_cluster_spec(worker_hosts, ps_hosts):
+    cluster_spec = tf.train.ClusterSpec({
     "worker": worker_hosts,
     "ps": ps_hosts
     })
-
-    if task_index == 0:
+    return cluster_spec
+    
+    
+def get_task_index(proc_id, job_name, num_ps):
+    
+    if job_name == "ps":
+        task_index = proc_id
+    elif job_name == "worker"
+        #expects a task_index for workers that starts at 0
+        task_index = proc_id - num_ps
+    return task_index
+    
+    
+def get_slurm_proc_variables():
+    proc_id  = int( os.environ['SLURM_PROCID'] )
+    num_procs     = int( os.environ['SLURM_NPROCS'] )
+    return proc_id, num_procs
+    
+def get_job_name(proc_id, num_ps):
+    if proc_id < num_ps:
         job_name = "ps"
     else:
         job_name = "worker"
-        #expects a task_index for worker
-        task_index -= 1
-    # Create and start a server for the local task.
-    server = tf.train.Server(cluster,
-                           job_name=job_name,
-                           task_index=task_index)
-    return cluster, server, task_index, n_tasks, job_name
+    return job_name
+    
+    
+def get_parameter_server_and_worker_hosts(hostlist, num_ps=1):
+    """assumes num_ps nodes used for parameter server (one ps per node)
+    and rest of nodes used for workers"""
+    ps_hosts = hostlist[:num_ps]
+    worker_hosts = hostlist[num_ps:]
+    return ps_hosts, worker_hosts
+    
+def get_allowed_port():
+    allowed_port = 22222
+    return allowed_port
+
+
+def get_all_nodes():
+    return expand_nodelist( os.environ['SLURM_NODELIST'])
     
 
-def setup_local_cluster(num_workers):
-    port_list = range(8000,9000)
-    random.shuffle(port_list)
-    tf_hostlist = [ (os.environ["HOSTNAME"] + ":%i" % port) for port in
-                port_list]
-    
-    
-    ps_hosts = [tf_hostlist[0]]
-    worker_hosts = tf_hostlist[1:num_workers+1]
-    job_name = sys.argv[2]
-    task_index  = int(sys.argv[3])
-    print "the task index is: ", task_index 
-    n_tasks     = len(port_list)
-    
-    cluster = tf.train.ClusterSpec({
-    "worker": worker_hosts,
-    "ps": ps_hosts
-    })
-    
-    # Create and start a server for the local task.
-    server = tf.train.Server(cluster,
-                           job_name=job_name,
-                           task_index=task_index)
-    
+def expand_nodelist(node_string):
+    pref, suff  = node_string.split('[')
 
-    return cluster, server, task_index, n_tasks, job_name
+    suff = suff. split(']')[0].split(',')
+    nodes =[]
+    for s in suff:
+        if '-' not in s:
+            nodes.append("%s%s" % (pref, s))
+            continue
+        beg,end = s.split('-')
+        num_len=len(beg)
+        for id in range(int(beg),int(end) + 1):
+            j= "%s%0" + str(num_len) + "d"
+            nodes.append(j % (pref, id))
+    return nodes
 
-def setup_cluster(typ_, num_workers=2):
-    if typ_ == "slurm":
-        return setup_slurm_cluster()
-    else:
-        return setup_local_cluster(2)
+if __name__ == "__main__":
+    node_string = "nid22[8,11-99,101-120]"
+    nodes = expand_nodelist(node_string)
+    print nodes
 
